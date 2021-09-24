44afaeb97ae9d53011056cf3c84ddaa2771d7437
suryamp97 <85583276+suryamp97@users.noreply.github.com>
2021-09-24 19:53:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, render_template ,jsonify
-from flaskext.mysql import MySQL
+import mysql.connector
 import tweepy
 app = Flask(__name__)
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'test'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = ''
+# app.config['MYSQL_DATABASE_DB'] = 'test'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# mysql.init_app(app)
 
 
 auth = tweepy.OAuthHandler("AC7WkBbTptQM1F9vPvaWMnPu0", "6Kv4MuuLHX3FvB0PxNLZaa1ajyIIrnGqVQriBvmYzGD8u2fD3O")
@@ -18,8 +18,14 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 @app.route("/")
 def main():
-    conn = mysql.connect()
-    cur = conn.cursor()
+    config = {
+        'user': 'root',
+        'password': '',
+        'host': 'localhost',
+        'database': 'test'
+    }
+    connection = mysql.connector.connect(**config)
+    cur = connection.cursor()
     operation= 'select * from history order by id desc limit 10'
     cur.execute(operation)
     data=cur.fetchall()
@@ -38,8 +44,14 @@ def main():
 def srch():
     if request.method == "POST":
         kw = request.form.get("search")
-        conn = mysql.connect()
-        cur = conn.cursor()
+        config = {
+            'user': 'root',
+            'password': '',
+            'host': 'localhost',
+            'database': 'test'
+        }
+        connection = mysql.connector.connect(**config)
+        cur = connection.cursor()
         x=0
         cur.execute("INSERT INTO History(id, keyw) VALUES (%s, %s)", (x, kw))
         conn.commit()
