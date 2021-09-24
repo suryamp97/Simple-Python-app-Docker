195186730dc723912499d77e310026d921344d4e
suryamp97 <85583276+suryamp97@users.noreply.github.com>
2021-09-24 16:33:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
   
+@app.route('/', methods =["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+       first_name = request.form.get("fname")
+       # getting input with name = lname in HTML form 
+       last_name = request.form.get("lname") 
+       return "Your name is "+first_name + last_name
+    return render_template("form.html")
+
+
 if __name__ == "__main__":
     app.run()
