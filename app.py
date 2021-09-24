db0a7631827258b62499f44ae7e93ec00853f90d
suryamp97 <85583276+suryamp97@users.noreply.github.com>
2021-09-24 15:16:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,4 +8,4 @@ def main():
     return "Welcome!"
   
 if __name__ == "__main__":
-    app.run()
+    return render_template('index.html')
